b096dbbc42af35e2b7fe4972d5fb6e3d5dbf9846
Robert Romero Medina <enanow41romero@gmail.com>
2021-05-16 16:23:00 -0400

    proyecto completo

---
+++ src/App.vue
@@ -0,0 +1,485 @@
+<script>
+import ModalResultado from "./components/ModalResultado.vue";
+import axios from "axios";
+export default {
+  name: "App",
+  components: {
+    ModalResultado,
+  },
+  data() {
+    return {
+      countrys: [],
+      // codigo pais ramdom
+      county_code_ramdom: null,
+      country_selecionado: null,
+      three_countrys_ramdom: [],
+
+      objetoPregunta: {
+        tipo: null,
+        pregunta: null,
+        respuesta: null,
+        otras_respuestas: null,
+      },
+      respuesta_selecionada: null,
+      selecionado_correcto: false,
+      respuestas_correctas: 0,
+      mostrar_resultados: false,
+
+      isLoading: false,
+    };
+  },
+
+  created() {
+    this.getCountrys();
+  },
+
+  methods: {
+    // traer todos los paies
+    async getCountrys() {
+      this.isLoading = true;
+      const urlAll = "https://restcountries.eu/rest/v2/all";
+
+      try {
+        const respuesta = await axios.get(urlAll);
+        this.countrys = respuesta.data;
+        this.getCodeRamdon();
+      } catch (error) {
+        console.error(error, "error al traer todos los paises");
+      }
+    },
+
+    // buscar y selecionar un pais ramdon
+    getCodeRamdon() {
+      try {
+        let countrys = [];
+        this.countrys.map((res) => {
+          countrys.push(res.alpha2Code);
+        });
+        const item = countrys[Math.floor(Math.random() * countrys.length)];
+        this.county_code_ramdom = item;
+
+        this.getCountrySelect();
+      } catch (error) {
+        console.error(error, "Error al mapear y selecionar el codigo pais ramdon");
+      }
+    },
+
+    // buscar pais selecionado ramdom y almacenar pais ramdon y su respuesta correcta
+    async getCountrySelect() {
+      this.isLoading = true;
+      const url = `https://restcountries.eu/rest/v2/alpha/${this.county_code_ramdom}`;
+
+      try {
+        const respuesta = await axios.get(url);
+        this.country_selecionado = respuesta.data;
+        this.buscarRespuestasIncorrectas();
+      } catch (error) {
+        console.error(error, "error al buscar el pais por codigo");
+      }
+    },
+
+    // buscar paies 3 paises ramdon (respuestas incorrectas)
+    // buscar respuestas incorrectas
+    async buscarRespuestasIncorrectas() {
+      this.isLoading = true;
+      let paises = [];
+
+      // conseguir el codigo de 3 paies
+      for (var i = 0; i < 3; i++) {
+        let countrys = [];
+        this.countrys.map((res) => {
+          countrys.push(res.alpha2Code);
+        });
+
+        // buscar los codigos ramdom
+        const item = countrys[Math.floor(Math.random() * countrys.length)];
+        paises.push(item);
+      }
+
+      if (paises.length > 0) {
+        // buscar los paises ramdoms
+        const url = `https://restcountries.eu/rest/v2/alpha?codes=${paises[0]};${paises[1]};${paises[2]}`;
+        const respuesta = await axios.get(url);
+        this.three_countrys_ramdom = respuesta.data;
+
+        this.tipopreguntaRamdom();
+      }
+    },
+
+    // selecionar tipo de pregunta ramdom
+    tipopreguntaRamdom() {
+      // tipos de preguntas:
+      // capitales, bandera, idioma, alphacode
+
+      const tipos = ["capital", "bandera"];
+      const item = tipos[Math.floor(Math.random() * tipos.length)];
+
+      console.log(item, "tipo pregunta");
+
+      this.construirPregunta(item);
+    },
+
+    // construir pregunta
+    construirPregunta(tipo) {
+      this.isLoading = true;
+      let respuestas = [];
+
+      const tipo_pregunta = tipo;
+
+      switch (tipo_pregunta) {
+        // tipo capital
+        case "capital":
+          this.three_countrys_ramdom.map((res) => {
+            respuestas.push(res.capital);
+          });
+          respuestas.push(this.country_selecionado.capital);
+
+          this.objetoPregunta = {
+            tipo: tipo_pregunta,
+            pregunta: `Select capital of the ${this.country_selecionado.name}`,
+            respuesta: this.country_selecionado.capital,
+            todas_respustas: respuestas.sort(() => Math.random() - 0.5),
+          };
+
+          console.log("capital");
+          break;
+
+        // tipo bandera
+        case "bandera":
+          this.three_countrys_ramdom.map((res) => {
+            respuestas.push(res.name);
+          });
+          respuestas.push(this.country_selecionado.name);
+
+          this.objetoPregunta = {
+            tipo: tipo_pregunta,
+            pregunta: `Which country does this flag belong to?`,
+            respuesta: this.country_selecionado.name,
+            todas_respustas: respuestas.sort(() => Math.random() - 0.5),
+            bandera: this.country_selecionado.flag,
+          };
+
+          break;
+        default:
+          console.log("default capital");
+          break;
+      }
+      this.isLoading = false;
+    },
+
+    // selecionar respuesta
+    selecionarRespuesta(dato) {
+      this.respuesta_selecionada = dato;
+
+      // si el usuario seleciono correctamente la respuesta
+      if (this.respuesta_selecionada == this.objetoPregunta.respuesta) {
+        this.selecionado_correcto = true;
+        // incrementar el puntaje
+        this.respuestas_correctas++;
+      }
+      // de lo contrario
+      else {
+        this.selecionado_correcto = false;
+      }
+      console.log(this.selecionado_correcto);
+    },
+
+    // validar respuesta
+    siguientePaso() {
+      // si la respuesta fue correcta, seguir con las demas preguntas
+      if (this.selecionado_correcto) {
+        this.resetear();
+      }
+      // si la respuesta fue incorrect, mostrar resultados
+      else {
+        this.mostrar_resultados = true;
+      }
+    },
+
+    // resetear
+    resetear() {
+      this.countrys = [];
+      this.county_code_ramdom = null;
+      (this.three_countrys_ramdom = []),
+        (this.objetoPregunta = {
+          tipo: null,
+          pregunta: null,
+          respuesta: null,
+          otras_respuestas: null,
+        });
+
+      this.respuesta_selecionada = null;
+      this.selecionado_correcto = null;
+      this.mostrar_resultados = false;
+
+      this.getCountrys();
+    },
+
+    // se ejecuta cuando el usuario halla selecionado algo mal y le de a try again
+    tryAain() {
+      this.resetear();
+      this.respuestas_correctas = 0;
+    },
+
+    // selecionar pregunta ramdom
+  },
+};
+</script>
+<template>
+  <div id="app">
+    <div class="container">
+      <!--container modall--->
+      <div class="container_modal">
+        <h1>Country Quiz</h1>
+        <!---modal preguntas-->
+        <div class="modal" :class="isLoading ? 'flex' : ''" v-if="!mostrar_resultados">
+          <template v-if="!isLoading">
+            <div class="modal__image">
+              <img src="./assets/init.svg" alt="" />
+            </div>
+            <!--tipo bandera--->
+            <div v-if="objetoPregunta.tipo == 'bandera'">
+              <img :src="objetoPregunta.bandera" width="100px" alt="" />
+            </div>
+            <!--fin tipo bandera--->
+            <div class="modal__title">
+              <h3>{{ objetoPregunta.pregunta }}</h3>
+            </div>
+            <div class="modal__respuestas">
+              <template v-if="true">
+                <template v-for="(res, index) in objetoPregunta.todas_respustas">
+                  <div
+                    class="__respuesta"
+                    :key="index"
+                    @click="selecionarRespuesta(res)"
+                    :class="
+                      selecionado_correcto
+                        ? respuesta_selecionada == res
+                          ? 'correct'
+                          : ''
+                        : respuesta_selecionada == res
+                        ? 'incorrect'
+                        : ''
+                    "
+                  >
+                    <span>{{ index + 1 }}</span>
+                    {{ res }}
+                    <template
+                      v-if="
+                        respuesta_selecionada
+                          ? selecionado_correcto
+                            ? res == objetoPregunta.respuesta
+                            : ''
+                          : false
+                      "
+                    >
+                      <i class="far fa-check-circle"></i>
+                    </template>
+                    <template
+                      v-if="
+                        respuesta_selecionada
+                          ? !selecionado_correcto
+                            ? respuesta_selecionada == res
+                            : ''
+                          : false
+                      "
+                    >
+                      <i class="far fa-times-circle"></i>
+                    </template>
+                    <!---->
+                    <template
+                      v-if="
+                        respuesta_selecionada != null
+                          ? selecionado_correcto == false
+                            ? res == objetoPregunta.respuesta
+                            : ''
+                          : ''
+                      "
+                    >
+                      <i class="far fa-check-circle" style="color: #60bf88"></i>
+                    </template>
+                  </div>
+                </template>
+              </template>
+            </div>
+            <div style="text-align: right" v-if="respuesta_selecionada">
+              <button class="boton" @click="siguientePaso">Next</button>
+            </div>
+            <br />
+            <div style="text-align: center">
+              <span class="link" @click="resetear">Cambiar pregunta</span>
+            </div>
+            <br />
+          </template>
+          <div style="text-align: center" v-if="isLoading">
+            <div class="lds-dual-ring"></div>
+          </div>
+        </div>
+        <!--fin modal preguntas-->
+
+        <ModalResultado
+          v-if="mostrar_resultados"
+          :correctas="respuestas_correctas"
+          @tryAain="tryAain"
+        />
+      </div>
+      <!--fin container modal-->
+    </div>
+  </div>
+</template>
+<style>
+body {
+  margin: 0;
+}
+#app {
+  width: 100%;
+  min-height: 100vh;
+  font-family: sans-serif !important;
+  color: #2c3e50;
+  background-image: url("./assets/background.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+.container {
+  width: 100%;
+  min-height: inherit;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+}
+.container_modal h1 {
+  text-align: left;
+  color: white;
+  margin: 0;
+  padding-bottom: 10px;
+}
+.container .modal {
+  width: 350px;
+  min-height: 300px;
+  background: white;
+  border-radius: 20px;
+  padding: 1em 2em;
+  text-align: left;
+}
+
+.flex {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.modal__image {
+  text-align: right;
+}
+.modal__image img {
+  width: 150px;
+  margin-top: -5em;
+  margin-right: -2em;
+}
+.__respuesta {
+  width: 300px;
+  background: white;
+  border: 1px solid rgba(96, 102, 208, 0.7);
+  padding: 1em;
+  margin: 0;
+  border-radius: 12px;
+  color: rgba(96, 102, 208, 0.7);
+  margin: 1em 0;
+  font-weight: 500;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+@media screen and (max-width: 550px) {
+  .modal__image img {
+    width: 30%;
+    margin-top: -5em;
+    margin-right: 0em;
+  }
+  .container .modal {
+    width: 100%;
+    margin: 5em auto;
+    padding: 4px;
+  }
+  .__respuesta {
+    width: 90%;
+    padding: 10px 5px;
+  }
+}
+
+.__respuesta i {
+  font-size: 20px;
+}
+
+.__respuesta:hover {
+  background: #f9a826 !important;
+  border: 1px solid transparent;
+  color: white;
+  transition: all 0.2s;
+}
+
+/*.__respuesta.select {
+  background: #f9a826 !important;
+  border: 1px solid transparent;
+  color: white;
+  transition: all 0.2s;
+} */
+.__respuesta.correct {
+  background: #60bf88;
+  border: 2px solid transparent;
+  color: white;
+}
+.__respuesta.incorrect {
+  background: #ea8282;
+  border: 2px solid transparent;
+  color: white;
+}
+
+.__respuesta span {
+  padding-left: 0.5em;
+  padding-right: 0.8em;
+}
+
+.boton {
+  padding: 1em;
+  height: 3em;
+  border: none;
+  background: #f9a826;
+  border-radius: 12px;
+  font-weight: bold;
+  color: white;
+  font-family: sans-serif;
+  cursor: pointer;
+}
+.link {
+  color: #6c5ce7;
+  cursor: pointer;
+}
+
+.lds-dual-ring {
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+}
+.lds-dual-ring:after {
+  content: " ";
+  display: block;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border-radius: 50%;
+  border: 6px solid #6c5ce7;
+  border-color: #6c5ce7 transparent #6c5ce7 transparent;
+  animation: lds-dual-ring 1.2s linear infinite;
+}
+@keyframes lds-dual-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
---
+++ src/components/ModalResultado.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="modal-resultado">
+      <img src="../assets/winner.svg" alt="" />
+      <h1>Results</h1>
+      <p>
+        You got
+        <span :class="correctas > 0 ? 'bien' : 'mal'">{{ correctas }}</span> correct
+        answers
+      </p>
+      <button @click="resetear">Try again</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    correctas: Number,
+  },
+  methods: {
+    resetear() {
+      this.$emit("tryAain");
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.modal-resultado {
+  width: 350px;
+  min-height: 300px;
+  background: white;
+  border-radius: 20px;
+  padding: 1em 2em;
+  text-align: center;
+}
+.modal-resultado h1 {
+  padding-top: 1em;
+  color: #1d355d;
+  text-align: center;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 40px;
+}
+.modal-resultado p span {
+  color: #6fcf97;
+  font-size: 2em;
+  font-weight: bold;
+}
+.bien {
+  color: #6fcf97 !important;
+}
+.mal {
+  color: red !important;
+}
+button {
+  padding: 5px 2em;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 27px;
+  border-radius: 8px;
+  color: #1d355d;
+  border: 1px solid #1d355d;
+  cursor: pointer;
+}
+@media screen and (max-width: 550px) {
+  .modal-resultado {
+    width: 90%;
+  }
+}
+</style>
